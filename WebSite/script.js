b0af272f84b7848a3c9af2e6d409f0190f58005f
Sorrger <Sorrger@gmail.com>
2024-12-23 00:27:47 +0100

    Main game html and css site updated and added some basic js

---
+++ WebSite/script.js
@@ -0,0 +1,10 @@
+function onStart(){
+    //ładowanie z pliku wartości brightnes
+    var slider = document.getElementsByClassName('brightnesSlider')[0];
+    if (slider) {
+        slider.value = 66; // tu bedzie sie bralo z pliku wartosci i dawalo tu
+    }
+}
+function startGame(){
+
+}
